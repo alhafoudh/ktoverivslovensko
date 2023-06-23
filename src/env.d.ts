ad2b3c06870b5109393ad6bca1117ad133a5b0c0
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2023-06-23 19:22:36 +0200

    Create and configure project

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly GTAG_CODE?: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+declare global {
+  let dataLayer: Record<string, any>[];
+}
+
+export {};
