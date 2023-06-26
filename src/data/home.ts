64317c2acdb280175357734b83403e5c7ce4e12c
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2023-06-26 17:15:11 +0200

    Some fixes from call

---
+++ src/data/home.ts
@@ -0,0 +1,128 @@
+import type { Belief, Supporter } from "../env";
+
+export const beliefs: Belief[] = [
+  {
+    image: "https://placehold.co/600x400",
+    name: "Ferko Mrkvička",
+    email: "ferko.mrkvicka@gmail.com",
+    city: "Bratislava",
+    text: "Proin sed turpis scelerisque ex tincidunt tempus non et turpis. Mauris laoreet ultrices pharetra. Proin sed elit nunc.",
+  },
+  {
+    image: "https://placehold.co/600x400",
+    name: "Ferko Mrkvička",
+    email: "ferko.mrkvicka@gmail.com",
+    city: "Bratislava",
+    text: "Proin sed turpis scelerisque ex tincidunt tempus non et turpis. Mauris laoreet ultrices pharetra. Proin sed elit nunc.",
+  },
+  {
+    image: "https://placehold.co/600x400",
+    name: "Ferko Mrkvička",
+    email: "ferko.mrkvicka@gmail.com",
+    city: "Bratislava",
+    text: "Proin sed turpis scelerisque ex tincidunt tempus non et turpis. Mauris laoreet ultrices pharetra. Proin sed elit nunc.",
+  },
+  {
+    image: "https://placehold.co/600x400",
+    name: "Ferko Mrkvička",
+    email: "ferko.mrkvicka@gmail.com",
+    city: "Bratislava",
+    text: "Proin sed turpis scelerisque ex tincidunt tempus non et turpis. Mauris laoreet ultrices pharetra. Proin sed elit nunc.",
+  },
+  {
+    image: "https://placehold.co/600x400",
+    name: "Ferko Mrkvička",
+    email: "ferko.mrkvicka@gmail.com",
+    city: "Bratislava",
+    text: "Proin sed turpis scelerisque ex tincidunt tempus non et turpis. Mauris laoreet ultrices pharetra. Proin sed elit nunc.",
+  },
+  {
+    image: "https://placehold.co/600x400",
+    name: "Ferko Mrkvička",
+    email: "ferko.mrkvicka@gmail.com",
+    city: "Bratislava",
+    text: "Proin sed turpis scelerisque ex tincidunt tempus non et turpis. Mauris laoreet ultrices pharetra. Proin sed elit nunc.",
+  },
+  {
+    image: "https://placehold.co/600x400",
+    name: "Ferko Mrkvička",
+    email: "ferko.mrkvicka@gmail.com",
+    city: "Bratislava",
+    text: "Proin sed turpis scelerisque ex tincidunt tempus non et turpis. Mauris laoreet ultrices pharetra. Proin sed elit nunc.",
+  },
+  {
+    image: "https://placehold.co/600x400",
+    name: "Ferko Mrkvička",
+    email: "ferko.mrkvicka@gmail.com",
+    city: "Bratislava",
+    text: "Proin sed turpis scelerisque ex tincidunt tempus non et turpis. Mauris laoreet ultrices pharetra. Proin sed elit nunc.",
+  },
+];
+
+export const personInitiators = [
+  "Lucia Pašková",
+  "Zuzana Suchová",
+  "Michaela Benedigova",
+  "Juraj Bartoš",
+  "Alžbeta Brozmanova",
+  "Dominika Hradiská",
+  "Romana Umrianová",
+  "Milan Šemelák",
+];
+
+export const companyInitiators = [
+  "Curaprox",
+  "Donio",
+  "Seesame",
+  "Free Andy",
+  "Platforma dobrovoľníckych centier a organizácií",
+  "Kto pomôže Slovensku",
+  "Kto pomôže Ukrajine",
+  "Ide nám o život",
+  "My sme les",
+  "Growni",
+  "Nexteria",
+];
+
+export const supporters: Supporter[] = [
+  {
+    name: "Organizácia",
+    amount: "100 000,00 €",
+    note: "finančná pomoc",
+  },
+  {
+    name: "Organizácia",
+    amount: "100 000,00 €",
+    note: "finančná pomoc",
+  },
+  {
+    name: "Organizácia",
+    amount: "100 000,00 €",
+    note: "finančná pomoc",
+  },
+  {
+    name: "Organizácia",
+    amount: "100 000,00 €",
+    note: "finančná pomoc",
+  },
+  {
+    name: "Organizácia",
+    amount: "100 000,00 €",
+    note: "finančná pomoc",
+  },
+  {
+    name: "Organizácia",
+    amount: "100 000,00 €",
+    note: "finančná pomoc",
+  },
+  {
+    name: "Organizácia",
+    amount: "100 000,00 €",
+    note: "finančná pomoc",
+  },
+  {
+    name: "Organizácia",
+    amount: "100 000,00 €",
+    note: "finančná pomoc",
+  },
+];
