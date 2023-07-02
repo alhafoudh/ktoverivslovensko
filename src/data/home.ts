706c9a023db0c9ea15cf514f0e2cce31eb26af28
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2023-07-02 21:39:47 +0200

    Update content

--- src/data/home.ts
+++ src/data/home.ts
@@ -7,7 +7,6 @@ export const personInitiators = [
   "Juraj Bartoš",
   "Alžbeta Brozmanová Gregorová",
   "Dominika Hradiská",
-  "Romana Umrianová",
   "Milan Šemelák",
 ];
 
@@ -20,9 +19,9 @@ export const companyInitiators = [
   "Kto pomôže Slovensku",
   "Kto pomôže Ukrajine",
   "Ide nám o život",
-  "My sme les",
   "Growni",
   "Nexteria",
+  "ADMA",
 ];
 
 export const supporters: Supporter[] = [
@@ -72,8 +71,25 @@ export const donationUrl = "https://www.donio.sk/prispevok/2479";
 
 export const donationAmounts = [10, 25, 50];
 
-export const donationUrlWithAmount = (amount: number | '') =>
+export const donationUrlWithAmount = (amount: number | "") =>
   `https://www.donio.sk/prispevok/2479?amount=${amount}`;
 
 export const beliefModalFormUrl =
   "https://docs.google.com/forms/d/e/1FAIpQLSeyO4pwS0698OYq5DiOdEI7xhSHsIMlxl0R1hYMP48efYqNvA/viewform";
+
+export const generatePollCalendarUrl = (
+  dateFrom: string,
+  dateTo: string,
+  title: string,
+  description: string
+): string => {
+  let params = new URLSearchParams({
+    action: "TEMPLATE",
+    text: title,
+    dates: `${dateFrom}/${dateTo}`,
+    details: description,
+    sf: "true",
+    output: "xml",
+  });
+  return `https://www.google.com/calendar/render?${params.toString()}`;
+};
