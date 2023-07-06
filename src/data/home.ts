395551bde61caea2fbaf4fc02450190dde864e9c
Ahmed Al Hafoudh <alhafoudh@freevision.sk>
2023-07-06 20:58:13 +0200

    Cleanup

--- src/data/home.ts
+++ src/data/home.ts
@@ -77,23 +77,3 @@ export const donationAmounts = [10, 25, 50];
 
 export const donationUrlWithAmount = (amount: number | "") =>
   `https://www.donio.sk/prispevok/2479?amount=${amount}`;
-
-export const beliefModalFormUrl =
-  "https://docs.google.com/forms/d/e/1FAIpQLSeyO4pwS0698OYq5DiOdEI7xhSHsIMlxl0R1hYMP48efYqNvA/viewform";
-
-export const generatePollCalendarUrl = (
-  dateFrom: string,
-  dateTo: string,
-  title: string,
-  description: string
-): string => {
-  let params = new URLSearchParams({
-    action: "TEMPLATE",
-    text: title,
-    dates: `${dateFrom}/${dateTo}`,
-    details: description,
-    sf: "true",
-    output: "xml",
-  });
-  return `https://www.google.com/calendar/render?${params.toString()}`;
-};
