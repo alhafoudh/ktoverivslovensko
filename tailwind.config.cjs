bf33827780c9c33dc65fea402e53045277a63cfe
Milan Marcinco <mmsprava@gmail.com>
2023-07-17 10:03:55 +0200

    Configure fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,29 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-  ],
-  darkMode: 'class',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     fontFamily: {
-      sans: ['Manrope Variable'],
+      sans: [
+        "Roboto",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        "Arial",
+        '"Noto Sans"',
+        "sans-serif",
+      ],
+      accent: "Antonio",
     },
     extend: {
       colors: {
-        primary: '#0000ff',
-        secondary: '#ff0000',
-        accent: '#e6e6ff',
-        label: '#8080ff',
-        separator: '#9999ff',
+        primary: "#0000ff",
+        secondary: "#ff0000",
+        accent: "#e6e6ff",
+        label: "#8080ff",
+        separator: "#9999ff",
       },
       scale: {
-        '-100': '-1',
-      }
+        "-100": "-1",
+      },
     },
     screens: {
-      'tablet': '834px',
-      'desktop': '1280px',
-    }
+      tablet: "834px",
+      desktop: "1280px",
+    },
   },
   plugins: [],
-}
+};
