9e178e07dbbf2577c2abc2c7a794c11e4d3ae0ea
Milan Marcinco <mmsprava@gmail.com>
2023-07-20 15:04:08 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  site: "https://www.zapnisapreslovensko.sk/",
+  integrations: [
+    tailwind(),
+    alpinejs(),
+    sitemap({
+      filter: page => !["https://www.zapnisapreslovensko.sk/uikit/"].includes(page),
+    }),
+  ],
 });
